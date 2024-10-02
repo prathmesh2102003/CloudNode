3bb3959d2268f8d05c45a1b454c2ef6f8f6a0685
prathmesh2102003 <prathmeshamale646@gmail.com>
2024-10-03 00:39:12 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,14 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('./app');
-const { expect } = chai;
-
+const expect = chai.expect;
 chai.use(chaiHttp);
-
-describe('Real-time Application Test Suite', () => {
-  it('should return welcome message at "/" route', (done) => {
+describe('Server', () => {
+ it('responds with status 200', done => {
     chai.request(server)
       .get('/')
       .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.text).to.include("Welcome! This CI/CD pipeline (Node.js) was deployed by Prathmesh using GitHub Actions!!!");
-        done();
-      });
-  });
-
-  it('should return 404 for invalid route', (done) => {
-    chai.request(server)
-      .get('/invalid')
-      .end((err, res) => {
-        expect(res).to.have.status(404);
-        expect(res.text).to.equal("404 Not Found\n");
+        expect(res.status).to.equal(200);
         done();
       });
   });
