3a447fa989068578faf7af7661fd5e91c83bcc75
prathmesh2102003 <prathmeshamale646@gmail.com>
2024-10-02 16:14:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end("This CI/CD pipeline (using node.js) deployed by Prathmesh using GitHub_Action!!!\n'");
+    res.end("This CI/CD pipeline (node.js) deployed by Prathmesh using GitHub_Action!!!\n'");
 });
 server.listen(port, () => {
     console.log(`Server running on port ${port}/`);
